72f2e4ada6199595976a5a9687814d68878ca04a
dhanush72 <dhanuraj7258@gmail.com>
2021-04-12 12:03:42 +0530

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
+
 module.exports = {
-  mode: "development",
+  mode: mode,
 
   module: {
     rules: [
@@ -12,6 +18,8 @@ module.exports = {
       },
     ],
   },
+
+  devtool: "source-map",
 
   devServer: {
     contentBase: "./dist", //relative path
